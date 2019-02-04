2c4d67ff5c6500b90fbcbd910c0d8930fd84ffb5
Kamila <malikovakamila2000@gmail.com>
2019-02-03 23:28:59 -0800

    first commit

---
+++ Week1/Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); // ввод числа и конвертирование ее в тип integer
+            int[] a = new int[n]; //создание массива с элементами числового значения
+            int[] res = new int[n]; //создание второго массива
+            string[] s = Console.ReadLine().Split(); //создание массива с элементами типа стринга и разделение их через пробелы
+            int cnt = 0; //создание переменной счетчика
+            bool prime = true;  //создание переменной булевого типа
+            for(int i=0;i<n;++i) //создание цикла
+            {
+                prime = true; 
+                a[i] = Convert.ToInt32(s[i]); //конвертирую элемент стрингового массива и сохраняю как элемент массива типа integer
+                if (a[i] == 1) // если элемент равен одному пропустить его
+                    continue;
+                for (int j = 2; j < a[i]; ++j) // создание второго цикла
+                {
+
+                    if (a[i] % j == 0) // проверка на prime number
+                        prime = false; // если число делится на предыдущие на него числа, начиная от 2, то это число не прайм
+                }
+                    if (prime == true) // в противном случае мы записываем prime number как элемент нового массива 
+                    {
+                        res[cnt] = a[i];
+                        cnt++; 
+                    }
+
+                }
+   
+            Console.WriteLine(cnt); //выводим количество простых чисел
+            for(int i=0;i<cnt;++i)
+            {
+                Console.Write(res[i] + " "); //выводим на консоль эти простые числа через пробел в одной строке
+            }
+
+        }
+    }
+}
---
+++ Week1/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        public static void Double(string[] s, int n) //создаю метод для дублировния элементов массива
+        {
+            for(int i=0;i<n;i++)
+            {
+                Console.Write(s[i] + " " + s[i] + " "); //вывожу каждый элемент дважды 
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine()); //создаю переменную и даю значение
+            string[] s = Console.ReadLine().Split(); //создаю стринговый массив и разделяю его через пробелы
+            Double(s, n); //вызываю метод для дублирования элементов массива    
+        }
+    }
+}
