922e9711fe3590a126b1b9cba29e2e93e9d94161
Kamila <malikovakamila2000@gmail.com>
2019-02-08 11:44:10 -0800

    Week2

---
+++ Week2/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\Asus\source\repos\Week2\input.txt");
+            for(int i=0,j=text.Length-1;i<j; i++,j--)
+            {
+                if (text[i]!= text[j])
+                {
+                    Console.WriteLine("No");
+                    return;
+                }               
+            }
+            Console.WriteLine("Yes");
+        }
+    }
+}
---
+++ Week2/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fs = new FileInfo(@"C:\Users\Asus\source\repos\Week2\Task4\Text1.txt");
+            string path = @"C:\Users\Asus\source\repos\Week2\Text1.txt";
+            fs.CopyTo(path, true);
+            fs.Delete();
+        }
+    }
+}
