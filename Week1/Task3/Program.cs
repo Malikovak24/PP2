6a4318012f8d00dd5760d6054b2fc56daf8ab1d7
Kamila <malikovakamila2000@gmail.com>
2019-02-08 21:48:10 -0800

    Upd:Second commit

--- Week1/Task3/Program.cs
+++ Week1/Task3/Program.cs
@@ -8,19 +8,30 @@ namespace Task3
 {
     class Program
     {
-        public static void Double(string[] s, int n) //создаю метод для дублировния элементов массива
-        {
-            for(int i=0;i<n;i++)
-            {
-                Console.Write(s[i] + " " + s[i] + " "); //вывожу каждый элемент дважды 
-            }
-
-        }
+        
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine()); //создаю переменную и даю значение
-            string[] s = Console.ReadLine().Split(); //создаю стринговый массив и разделяю его через пробелы
-            Double(s, n); //вызываю метод для дублирования элементов массива    
+            int[] ar1 = new int[n]; //создаю массив с типом integer
+            int[] ar2 = new int[n*2]; //создаю массив с типом integer у которого размер в 2 раза больше предыдущего массива
+            string[] s = Console.ReadLine().Split(); //создаю стринговый массив
+            for(int i=0;i<n;i++)
+            {
+                ar1[i] = Convert.ToInt32(s[i]); //конвертирую элементы стрингового массива и записываю как элемент массива типа integer
+            }
+            int j = 0;
+            for(int i=0;i<ar1.Length;i++)
+            {
+                ar2[j++] = ar1[i]; //два раза выводим элементы массива
+                ar2[j++] = ar1[i];
+            }
+            for(int i=0;i<ar2.Length;i++)
+            {
+                Console.Write(ar2[i] + " "); //выводим элементы нового массива
+            }
+
+
+            
         }
     }
 }
