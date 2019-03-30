3ab1f7f5104a16a741502e155bdfd97b47cb33e3
Kamila <malikovakamila2000@gmail.com>
2019-03-29 19:18:24 -0700

    task1

---
+++ Week7/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] thread = new Thread[3];
+            thread[0] = new Thread(PrintInfo);
+            thread[0].Name = "Thread 1";
+            thread[1] = new Thread(PrintInfo);
+            thread[1].Name = "Thread 2";
+            thread[2] = new Thread(PrintInfo);
+            thread[2].Name = "Thread 3";
+            for(int i=0;i<3;i++)
+            {
+                thread[i].Start();
+            }
+        }
+        static void PrintInfo()
+        {
+            for(int i=0;i<3;i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+            }
+        }
+    }
+}
